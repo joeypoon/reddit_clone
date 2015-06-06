19605250ac24380111409e47d788b49f4c05dd73
Joey Poon <joey@joeypoon.com>
2015-06-06 15:52:13 -0500

    Add validation for links

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :title, presence: true,
-                  uniqueness: { scope: :user }
+  validates :title, presence: true
   validates :content, presence: true,
-                        format: { with: /https?:\/\/[\S]+/ }
+                        format: { with: /https?:\/\/[\S]+/ },
+                        uniqueness: true
   validates :user, presence: true
 
 end
